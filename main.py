339967073ddc220f5d765d3beb2a2cea3407a113
Dylan LLODRA <dylanllodrapro@gmail.com>
2022-10-24 21:33:13 +0200

    change main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #import module
-
 import csv 
 
+#variables
+
+
 # Nom du CSV à lire
-filename = "b3-c1-scripting-project-maurras-thibaut-llodra-dylan/Sample.csv"
-def list_csv() -> list:
+filename = "Sample.csv"
+def read_csv_to_list() -> list:
+    list_data = []
     with open(filename, 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
         next(csv_reader)
         for line in csv_reader:
-            print(line)
+            list_data.append(line)
+    return list_data
 
-list_csv()
-        
+data = read_csv_to_list()
+print(data)

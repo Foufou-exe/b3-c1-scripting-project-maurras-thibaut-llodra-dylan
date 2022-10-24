7563ab9dde9d3b18322c013146b8d658e6b97ca3
Dylan LLODRA <dylanllodrapro@gmail.com>
2022-10-24 23:36:59 +0200

    working step

--- main.py
+++ main.py
@@ -15,16 +15,20 @@ def parsing_line(line) -> list:
     list_element_line.pop(2)
     list_element_line.pop(2)
     list_element_line.pop(2)
-    #Supprimer toutes les lignes avec moins une cellule vide
+    #Verifie s'il n'ya pas de cellule vide
+    empty = False
     for element in list_element_line:
-        if element != "" :
-            #return de la list des elements de la ligne parser
-            return list_element_line
+        if len(element) == 0:
+            empty = True
+    if not empty:
+        #return de la list des elements de la ligne parser
+        return list_element_line
 
 def write_clean_list_to_csv(list_cleaned):
-    with open(fileout, 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows(list_cleaned)
+    if list_cleaned != []:
+        with open(fileout, 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(list_cleaned)
 
 def read_csv_line():
     liste_clean = []
@@ -32,36 +36,9 @@ def read_csv_line():
         csv_reader = csv.reader(csv_file, delimiter = ',')
         next(csv_reader)
         for line in csv_reader:
-            liste_clean.append(parsing_line(line))
+            liste_clean_temp = parsing_line(line)
+            if type(liste_clean_temp) is list:
+                liste_clean.append(liste_clean_temp)
     write_clean_list_to_csv(liste_clean)
 
-
-
-
-
-#### COMPREHENSION
-
-
-# A B C D E F G H I J 
-# 0 1 2 3 4 5 6 7 8 9
-#   0 1 2 3 4 5 6 7 8 
-#   0 1 - 2 3 4 5 6 7 
-#   0 1 - - 2 3 4 5 6 
-#   0 1 - - - 2 3 4 5 
-#   0 1 - - - - 2 3 4 
-#['8152'=A 0, '6160'=B 1, 'Alan Dominguez'=C 2, '3746'=D 3, '162.67'=E 4 , '115.99'=F 5, '2.5'=G 6, 'Alberta'=H 7, 'Telephones and Communication'=I 8, '0.57'=J 9]
-#Suppression des Colonnes A,D,E,F,G
-#garde des Colonnes B,C,H,I,J
-
-# 7145, A 
-# "Telephone Message Books with Fax/Mobile Section,  B
-# 4 1/4"" x 6""", C
-# Jill Matthias, D
-# 43846, E
-# -8.28, F
-# 3.6,2.2, G 
-# British Columbia, H
-# Paper, I 
-# 0.39 J
-
-#['Telephone Message Books with Fax/Mobile Section, 4 1/4" x 6"', 'Jill Matthias', '43846', 'British Columbia', 'Paper', '0.39']
+read_csv_line()

66e8e84e17f5788f028f66b076f4364954f9318c
Dylan LLODRA <dylanllodrapro@gmail.com>
2022-10-24 22:47:18 +0200

    code fonction move

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ filename = "Sample.csv"
 fileout = "clean.csv"
 
 #Fonction
-def read_csv_line():
-    liste_clean = []
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter = ',')
-        next(csv_reader)
-        for line in csv_reader:
-            liste_clean.append(parsing_line(line))
-    write_clean_list_to_csv(liste_clean)
-
 def parsing_line(line) -> list:
     #init de la variable tableau 'list_element_line' qui contien tout les items de la ligne csv
     list_element_line = line
@@ -29,14 +20,21 @@ def parsing_line(line) -> list:
         if element != "" :
             #return de la list des elements de la ligne parser
             return list_element_line
-    
+
 def write_clean_list_to_csv(list_cleaned):
     with open(fileout, 'w') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerows(list_cleaned)
 
+def read_csv_line():
+    liste_clean = []
+    with open(filename, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ',')
+        next(csv_reader)
+        for line in csv_reader:
+            liste_clean.append(parsing_line(line))
+    write_clean_list_to_csv(liste_clean)
 
-read_csv_line()
 
 
 

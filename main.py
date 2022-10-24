6c98128f8e9375b2e467a0b3823ff876e6e5768d
/Foufoudu34 <76502393+Foufou-exe@users.noreply.github.com>
2022-10-24 22:21:06 +0200

    Update main.py⚙️
    update

--- main.py
+++ main.py
@@ -16,9 +16,16 @@ def read_csv_to_list() -> list:
     return list_data
 
 def parsing_line(line) -> list:
-    line.pop(0)
-    line.pop(2)
-    line.pop(2)
-    line.pop(2)
-    line.pop(2)
-    return line
+    #init de la variable tableau 'list_element_line' qui contien tout les items de la ligne csv
+    list_element_line = line
+    #Suppression des Colonnes A,D,E,F,G
+    list_element_line.pop(0)
+    list_element_line.pop(2)
+    list_element_line.pop(2)
+    list_element_line.pop(2)
+    list_element_line.pop(2)
+    #Supprimer toutes les lignes avec moins une cellule vide
+    for element in list_element_line:
+        if element != "" :
+            #return de la list des elements de la ligne parser
+            return list_element_line

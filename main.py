ba10f07457ca22c7b3a0c934f09e18e627079915
Dylan LLODRA <dylanllodrapro@gmail.com>
2022-10-25 11:49:24 +0200

    renex

--- main.py
+++ main.py
@@ -3,23 +3,46 @@ import csv
 
 #variables
 filename = "conso-annuelles_v1.csv"
-fileout = "clean.csv"
+fileout = "conso-clean.csv"
 
 #Fonction
 def parsing_line(line) -> list:
     #init de la variable tableau 'list_element_line' qui contien tout les items de la ligne csv
     list_element_line = line
-    #Suppression des Colonnes A,D,E,F,G
-    
-    
+    #Suppression de la Colonnes ID_Logement
+    list_element_line.pop(1)
     #Verifie s'il n'ya pas de cellule vide
     empty = False
     for element in list_element_line:
         if len(element) == 0:
-            empty = True
+            empty = True 
     if not empty:
+        #Addition puis suppression des colonnes annees 1 et 2 puis resulta dans une nouvelle colonne
+        annee_total = float(list_element_line[1].replace(',','.').replace('-','0')) + float(list_element_line[2].replace(',','.').replace('-','0'))
+        list_element_line.pop(1)
+        list_element_line.pop(1)
+        list_element_line.insert(1, annee_total)
         #return de la list des elements de la ligne parser
         return list_element_line
+
+def sort_list_to_csv(list_all_data):
+    #init de la variable tableau 'list_all_data' qui contien tout les items de la ligne csv
+    list_all_element = list_all_data
+    #Tri par ordre alphabetique le type des elements de la list
+    dico_type = {}
+    for data_list in list_all_element:
+        if data_list[2] in dico_type:
+            print(data_list)
+            print(dico_type)
+            dico_type_temp = {dico_type[data_list[2]], dico_type[data_list[2]].append(data_list)}
+            print(dico_type_temp)
+            #dico_type[data_list[2]] = dico_type[data_list[2]]
+        else:
+            dico_type[data_list[2]] = [data_list]
+    #list_all_element = sorted(list_all_element, key=lambda inner_list: inner_list[2])
+    #Tri par ordre decroissant le total des années des elements de la list
+    #list_all_element = sorted(list_all_element, key=lambda inner_list: inner_list[1], reverse=True)
+    return list_all_element
 
 def write_clean_list_to_csv(list_cleaned):
     with open(fileout, 'w') as csv_file:
@@ -28,13 +51,14 @@ def write_clean_list_to_csv(list_cleaned):
 
 def read_csv_line():
     liste_clean = []
-    with open(filename, 'r') as csv_file:
+    #liste_clean.append(['Appareil suivi', 'ID logement', 'Consommation annuelle Total(AN1+AN2)', 'Type'])
+    with open(filename, 'r', encoding="latin-1") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ';')
         next(csv_reader)
         for line in csv_reader:
-            liste_clean_temp = parsing_line(line)
-            if type(liste_clean_temp) is list:
-                liste_clean.append(liste_clean_temp)
-    write_clean_list_to_csv(liste_clean)
+                liste_clean_temp = parsing_line(line)
+                if type(liste_clean_temp) is list:
+                    liste_clean.append(liste_clean_temp)
+    write_clean_list_to_csv(sort_list_to_csv(liste_clean))
 
 read_csv_line()
